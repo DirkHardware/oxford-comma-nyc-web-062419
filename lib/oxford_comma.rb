6836d4f1f2b8c4023e6b07394476ae7b924bc279
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 18:48:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,11 +2,11 @@ def oxford_comma(array)
   new_array1 = []
   new_array2 = []
   new_array3 = []
-  if array.length == 1
-    puts "1"
-  elsif array.length == 2
-    2array = array.insert(2, "and")
-    2array = array.join(" ")
-    2array
-  end
+  # if array.length == 1
+  #   puts "1"
+  # elsif array.length == 2
+  #   2array = array.insert(2, "and")
+  #   2array = array.join(" ")
+  #   2array
+  # end
 end
