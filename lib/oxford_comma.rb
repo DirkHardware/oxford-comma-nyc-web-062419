14bc6d4d28d5cfc9072bc27a233cd1da2de03458
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 18:43:34 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,7 @@
 def oxford_comma(array)
   if array.length == 1
     array
-  elsif
-    array.length == 2
+  elsif array.length == 2
     2array = array.insert(2, "and")
     2array = array.join(" ")
     2array
