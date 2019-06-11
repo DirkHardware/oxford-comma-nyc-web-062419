e523bd0ae210a06d5050763025dfb3cf74a55f28
Anderson Rolf <email.the.anderson@gmail.com>
2019-06-11 18:48:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,12 @@
 def oxford_comma(array)
-  new_array = []
-  # if array.length == 1
-  #   puts "1"
-  # elsif array.length == 2
-  #   2array = array.insert(2, "and")
-  #   2array = array.join(" ")
-  #   2array
-  # end
+  new_array1 = []
+  new_array2 = []
+  new_array3 = []
+  if array.length == 1
+    puts "1"
+  elsif array.length == 2
+    2array = array.insert(2, "and")
+    2array = array.join(" ")
+    2array
+  end
 end
